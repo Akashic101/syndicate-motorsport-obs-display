222c71a4e284856bdc1ef2bbecef984780037f6a
David Moll <david.leander.moll@gmail.com>
2024-02-10 15:48:43 +0100

    Added dark themed docs

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,12 @@
 import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    docs: {
+      theme: themes.dark,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
